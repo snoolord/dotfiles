9612543a5f38be7d5f5a87c61d755e3f8f2f13a5
Winston Zhao <winstonzhao@ucla.edu>
2023-04-27 13:44:56 -0700

    add setup script

---
+++ setup.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env ts-node
+import { exec } from "child_process"
+
+const homebrewApplicationInstallCommands = {
+  vsCode: "brew install --cask visual-studio-code",
+  raycast: "brew install --cask raycast",
+  adguard: "brew install --cask adguard",
+  mackup: "brew install mackup",
+  pixelSnap: "brew install --cask pixelsnap",
+  btt: "brew install --cask bettertouchtool",
+  googleDrive: "brew install --cask google-drive",
+  dropbox: "brew install --cask dropbox",
+  spotify: "brew install --cask spotify",
+  vlc: "brew install --cask vlc",
+  figma: "brew install --cask figma",
+  orion: "brew install --cask orion",
+  chrome: "brew install --cask google-chrome",
+  karabiner: "brew install --cask karabiner-elements",
+  discord: "brew install --cask discord",
+}
+
+Object.values(homebrewApplicationInstallCommands).forEach((command) => {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+  })
+})
